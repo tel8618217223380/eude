9c9ee5b5e0c5bba83633dab75cda9b86e530c2f6
Alex10336 <Alex10336@7e1166ec-126b-11df-b7f4-157485fe8b2f>
2010-02-08 14:22:11 +0000

    #191 login.tpl

--- trunk/last/tpl/lng/fr/login.lng.php
+++ trunk/last/tpl/lng/fr/login.lng.php
@@ -1,15 +1,28 @@
 <?php
 /*
  * @author Alex10336
- * Dernière modification: $Id: login.php 29 2010-02-06 00:39:19Z Alex10336 $
+ * Dernière modification: $Id$
  * @license GNU Public License 3.0 ( http://www.gnu.org/licenses/gpl-3.0.txt )
  * @license Creative Commons 3.0 BY-SA ( http://creativecommons.org/licenses/by-sa/3.0/deed.fr )
  *
  */
 
 $lng = array();
+// Script/login.php
 $lng['wronglogin'] = 'Mot de passe ou nom d\'utilisateur incorrect';
 $lng['wronglogin_log'] = 'Identification au Data Engine échoué.';
 $lng['session_lost'] = 'Session invalide !';
 $lng['no_axx'] = 'Vous n\'avez pas la permission d\'utilisation, Demander les acc&egrave;s a votre alliance';
 $lng['login_page_title'] = 'EU2: DataEngine, Identification';
+
+// tpl/login.tpl.php
+
+$lng['register'] = 'Enregistrer';
+$lng['signin'] = 'Connexion';
+$lng['player'] = 'Joueur';
+$lng['password'] = 'Mot de passe';
+
+$lng['newaccount'] = 'Créer un compte';
+$lng['newaccount_warn'] = 'La création d\'un compte est soumis a la validation de votre empereur';
+$lng['allreadyhaveone'] = 'Déjà un compte ?';
+
--- trunk/last/tpl/login.tpl.php
+++ trunk/last/tpl/login.tpl.php
@@ -13,16 +13,18 @@ $tpl->DoOutput($login_msg,$register);
 */
 class tpl_login extends output {
 	protected $BASE_FILE = '';
+	protected $lng = '';
 
 	public function __construct() {
 		$this->BASE_FILE = ROOT_URL."register.php";
+                $this->lng = language::getinstance()->GetLngBlock('login');
 
 		parent::__construct();
 	}
 
 	public function DoOutput($login_msg='',$register=false) {
 		$action = ($register) ? $this->BASE_FILE: '';
-		$btn_text = ($register) ? "Enregistrer": "Connexion";
+		$btn_text = ($register) ? $lng['register']: $lng['signin'];
 $out =<<<BASE
 <CENTER>
 	<table>
@@ -31,12 +33,12 @@ $out =<<<BASE
 	</tr>
 	<tr id='TRtableau'>
 		<Form name="LOG" method="post" action='{$action}'>
-		<td id='TDtableau'>Joueur :</td>
+		<td id='TDtableau'>{$this->lng['player']} :</td>
 		<td id='TDtableau'><input tabindex=1 id='INtableau' type="text" value="" name="login" /></td>
 		<td id='TDtableau' rowspan=2 style='valign=center'><input id='INBTtableau' type="submit" value="{$btn_text}" /></td>
 	</tr>
 	<tr id='TRtableau'>
-		<td id='TDtableau'>Pass :</td>
+		<td id='TDtableau'>{$lng['password']} :</td>
 		<td id='TDtableau'><input tabindex=2 id='INtableau' type="password" value="" name="mdp" /></td>
 	</tr>
 BASE;
@@ -60,14 +62,14 @@ LOGIN;
 	if (!$register && Config::CanRegister())
 $out .= <<<LOGIN
 	<tr id='titreTRtableau'>
-		<td colspan=3 id='titreTRtableau'><a href='register.php'>Créer un compte</a></td>
+		<td colspan=3 id='titreTRtableau'><a href='register.php'>{$lng['newaccount']}</a></td>
 	</tr>
 LOGIN;
 	elseif ($register)
 $out .= <<<REGISTER
 	<tr id='TRtableau'>
-		<td colspan=3 id='TDtableau' align=center>La création d'un compte est soumis a la validation de votre empereur<br/>
-			<input id='INBTtableau' type="button" value="Déjà un compte ?" Onclick="location.href='./logout.php'" /></td>
+		<td colspan=3 id='TDtableau' align=center>{$lng['newaccount_warn']}<br/>
+			<input id='INBTtableau' type="button" value="{$lng['allreadyhaveone']}" Onclick="location.href='./logout.php'" /></td>
 	</tr>
 REGISTER;
 
