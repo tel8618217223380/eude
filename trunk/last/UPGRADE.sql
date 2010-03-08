90e79876068422444d050167efb279881925cb6b
jc.kubiaczyk@free.fr <jc.kubiaczyk@free.fr@7e1166ec-126b-11df-b7f4-157485fe8b2f>
2010-03-08 20:23:51 +0000

    A modifier au besoin, sinon pas possible de crée la base de teste Beta

--- trunk/last/UPGRADE.sql
+++ trunk/last/UPGRADE.sql
@@ -2,3 +2,9 @@
 -- Fichier de MAJ SQL
 -- depuis 1.4.1
 --
+
+CREATE TABLE `SQL_PREFIX_Config` (
+`key` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+`perms` BIGINT NOT NULL ,
+`EmpireAllys` TEXT NOT NULL ,
+`EmpireEnnemy` TEXT NOT NULL ) ENGINE = MYISAM ;
