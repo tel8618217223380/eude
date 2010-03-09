94810819e2ccc1dd7b4ec6c7912f7abf022f295e
Alex10336 <Alex10336@7e1166ec-126b-11df-b7f4-157485fe8b2f>
2010-03-09 19:22:06 +0000

    fixes issue 27 Labels: +Milestone-141

--- last/UPGRADE.sql
+++ last/UPGRADE.sql
@@ -2,3 +2,9 @@
 -- Fichier de MAJ SQL
 -- depuis 1.4.1
 --
+ALTER TABLE `Membres`
+    CHANGE `carte_prefs` `carte_prefs` VARCHAR( 30 ) NOT NULL DEFAULT '',
+    CHANGE `Race` `Race` VARCHAR( 30 ) NOT NULL DEFAULT '',
+    CHANGE `ship` `ship` VARCHAR( 50 ) NOT NULL DEFAULT '',
+    CHANGE `Titre` `Titre` VARCHAR( 50 ) NOT NULL DEFAULT '',
+    CHANGE `GameGrade` `GameGrade` VARCHAR( 50 ) NOT NULL DEFAULT '';
