1b7e67b56a234741a248b4f1d2604b07ae3b42fe
Alex10336 <Alex10336@7e1166ec-126b-11df-b7f4-157485fe8b2f>
2010-03-16 16:11:10 +0000

    i18n: pl/en ownuniverse.lng upgraded

--- last/tpl/lng/pl/ownuniverse.lng.php
+++ last/tpl/lng/pl/ownuniverse.lng.php
@@ -15,8 +15,11 @@ $lng['page_title']     = 'EU2: Son univers';
 $lng['start_info']     = 'Lisez les infos sur le <b>i</b> pour commencer';
 $lng['data_error']     = 'Information collé non reconnue';
 $lng['control_center_error'] = 'Le centre de contrôle en premier !';
-$lng['control_center_ident'] = 'Zaopatrzenie ludności na dzień';
+$lng['universe_added'] = 'Informations de votre univers ajouté.';
+$lng['universe_updated'] = 'Informations de votre univers mise a jour.';
+$lng['universe_nochange'] = 'Aucune différence, donnée inchangée.';
 // partie détection, par block,
+$lng['control_center_ident'] = 'Zaopatrzenie ludności na dzień';
 $lng['block_planet_0'] = 'Opis';
 $lng['block_planet_1'] = 'Razem';
 $lng['block_coords_0'] = 'Współrzędne';
@@ -45,10 +48,49 @@ $lng['batiments']['tradepost']     = 'Placówka handlowa';
 $lng['batiments']['ressource']     = 'Fabryka surowców';
 
 $lng['planet_error'] = 'Cette planète ne fait pas partie de votre univers (voir centre de controle)';
-$lng['planet_ident'] = 'Podział surowców';
+$lng['planet_added']    = 'Information de la planète %s ajouté.';
+$lng['planet_nochange'] = 'Aucune différence, donnée inchangée.';
 // partie détection,
+$lng['planet_ident'] = 'Podział surowców';
 $lng['planet_key_0'] = 'Podział surowców';
 $lng['planet_key_1'] = 'Informacje planet';
 $lng['planet_key_2'] = 'Podział surowców';
 $lng['planet_key_3'] = 'Współrzędne';
 $lng['planet_key_4'] = 'Osłona wodna';
+
+// Tableau...
+$lng['tips_header'] = <<<tips_header
+            Coller ici les détails du '<b>Centre de contrôle</b>' puis '<b>Planètes</b>'<br/>
+(Ctrl+A puis Ctrl+C après avoir ouvert la page)<br/>
+<br/>
+Suivit après par planète un copier/coller de la page 'Aperçu' pour les informations complémentaire
+tips_header;
+
+$lng['header']            = 'Ajout de l\'info... (centre de contrôle niveau 2 mini)';
+$lng['btn_submit']        = 'Interpréter';
+$lng['btn_reset']         = 'Reset';
+
+$lng['row_concentration'] = 'Concentration';
+$lng['row_prod/h']        = 'Prod/h';
+$lng['row_stocks']        = 'Stocks';
+$lng['row_Total']         = 'Total';
+$lng['row_race_needed']   = 'Consommation';
+$lng['cols_planets']      = 'Planète(s)';
+$lng['cols_Total']        = 'Total';
+
+// bulles
+$lng['current_ress_row_1']=<<<crr1
+%s<br/>
+Sur planète: %s<br/>
+Dans le bunker: %s
+crr1;
+
+$lng['current_ress_row_2']=<<<crr2
+Sur planète: %s
+<br/>Dans le bunker: %s
+<br/>En sécurité: %s%%
+<br/>Utilisation bunker: %s%%
+crr2;
+
+
+
