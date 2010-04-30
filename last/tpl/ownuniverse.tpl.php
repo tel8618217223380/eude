3238351becb06c1a8c9d594d4e2f31d490581d85
Alex10336 <Alex10336@7e1166ec-126b-11df-b7f4-157485fe8b2f>
2010-04-30 15:37:37 +0000

    update issue 70
    recherche/ownuniverse

--- last/tpl/ownuniverse.tpl.php
+++ last/tpl/ownuniverse.tpl.php
@@ -24,43 +24,43 @@ class tpl_ownuniverse extends output {
         if ($include_form) {
             $bulle = bulle($this->lng['tips_header']);
             $out =<<<h
-	<TABLE id='imperium_header'>
+	<TABLE class="table_center table_nospacing color_row0 text_center">
 	<form name='data' method='post' action='{$this->BASE_FILE}'>
-	<TR id='imperium_header'>
-		<TD id='titreTDtableau' colspan=12><img {$bulle} src='%IMAGES_URL%help.png'/> {$this->lng['header']}</TD>
+	<TR>
+		<TD class="color_bigheader" colspan=12><img {$bulle} src='%IMAGES_URL%help.png'/> {$this->lng['header']}</TD>
 	</tr>
-	<TR id='imperium_header'>
-		<td id="TDtableau">&nbsp;</TD>
-		<TD id="TDtableau" colspan=8>
-			<TEXTAREA id="INTableau" cols="50" rows="4" name='importation'></TEXTAREA>
+	<TR>
+		<td>&nbsp;</TD>
+		<TD colspan=8>
+			<TEXTAREA class="color_row0" cols="50" rows="4" name='importation'></TEXTAREA>
 		</TD>		
-		<TD id="TDtableau" colspan=2><input id="INTableau" type=submit value='{$this->lng['btn_submit']}'><br/>
-                <input id="INTableau" type=button onclick="location.href='{$this->BASE_FILE}?reset={$_SESSION['_permkey']}';" value='{$this->lng['btn_reset']}'></td>
-		<td id="TDtableau">&nbsp;</TD>
+		<TD colspan=2><input class="color_row0" type=submit value='{$this->lng['btn_submit']}'><br/>
+                <input class="color_row0" type=button onclick="location.href='{$this->BASE_FILE}?reset={$_SESSION['_permkey']}';" value='{$this->lng['btn_reset']}'></td>
+		<td>&nbsp;</TD>
 	</tr>
         </form>
 h;
             $this->PushOutput($out);
         } else {
-            $this->PushOutput('<br/><TABLE id="imperium_header">');
+            $this->PushOutput('<br/><TABLE>');
         }
     }
 
     public function RowHeader() {
         $out=<<<h
-            <TR id='imperium_header'>
-		<td id="TDtableau">&nbsp;</TD>
-		<td id="TDtableau"><img src='%IMAGES_URL%Titane.png'>&nbsp;{$this->ress[0]['Nom']}</TD>
-		<td id="TDtableau"><img src='%IMAGES_URL%Cuivre.png'>&nbsp;{$this->ress[1]['Nom']}</TD>
-		<td id="TDtableau"><img src='%IMAGES_URL%Fer.png'>&nbsp;{$this->ress[2]['Nom']}</TD>
-		<td id="TDtableau"><img src='%IMAGES_URL%Aluminium.png'>&nbsp;{$this->ress[3]['Nom']}</TD>
-		<td id="TDtableau"><img src='%IMAGES_URL%Mercure.png'>&nbsp;{$this->ress[4]['Nom']}</TD>
-		<td id="TDtableau"><img src='%IMAGES_URL%Silicium.png'>&nbsp;{$this->ress[5]['Nom']}</TD>
-		<td id="TDtableau"><img src='%IMAGES_URL%Uranium.png'>&nbsp;{$this->ress[6]['Nom']}</TD>
-		<td id="TDtableau"><img src='%IMAGES_URL%Krypton.png'>&nbsp;{$this->ress[7]['Nom']}</TD>
-		<td id="TDtableau"><img src='%IMAGES_URL%Azote.png'>&nbsp;{$this->ress[8]['Nom']}</TD>
-		<td id="TDtableau"><img src='%IMAGES_URL%Hydrogene.png'>&nbsp;{$this->ress[9]['Nom']}</TD>
-		<td id="TDtableau">{$this->lng['cols_Total']}</TD>
+            <TR class="color_header spacing_header">
+		<td>&nbsp;</TD>
+		<td><img src='%IMAGES_URL%Titane.png'>&nbsp;{$this->ress[0]['Nom']}</TD>
+		<td><img src='%IMAGES_URL%Cuivre.png'>&nbsp;{$this->ress[1]['Nom']}</TD>
+		<td><img src='%IMAGES_URL%Fer.png'>&nbsp;{$this->ress[2]['Nom']}</TD>
+		<td><img src='%IMAGES_URL%Aluminium.png'>&nbsp;{$this->ress[3]['Nom']}</TD>
+		<td><img src='%IMAGES_URL%Mercure.png'>&nbsp;{$this->ress[4]['Nom']}</TD>
+		<td><img src='%IMAGES_URL%Silicium.png'>&nbsp;{$this->ress[5]['Nom']}</TD>
+		<td><img src='%IMAGES_URL%Uranium.png'>&nbsp;{$this->ress[6]['Nom']}</TD>
+		<td><img src='%IMAGES_URL%Krypton.png'>&nbsp;{$this->ress[7]['Nom']}</TD>
+		<td><img src='%IMAGES_URL%Azote.png'>&nbsp;{$this->ress[8]['Nom']}</TD>
+		<td><img src='%IMAGES_URL%Hydrogene.png'>&nbsp;{$this->ress[9]['Nom']}</TD>
+		<td class="spacing_row">{$this->lng['cols_Total']}</TD>
 	</TR>
 h;
         $this->PushOutput($out);
@@ -78,15 +78,17 @@ h;
     }
 
     public function Planet_Header($planet) {
+        $water_style = '';
         if ($planet['percent_water']) {
             $bulle = bulle($this->lng['planet_key_4']);
             $planet['percent_water'] .= '%';
+            $water_style = 'spacing_row1';
         } else $planet['percent_water'] = '&nbsp;';
         $out=<<<ph
-	<TR id='imperium_header'>
-		<td id="TDtableau" colspan=2>{$planet['Name']}: {$planet['Coord']}</td>
-		<td id="TDtableau" {$bulle}>{$planet['percent_water']}</td>
-		<td id="TDtableau" colspan=9>&nbsp;</td>
+	<TR class="color_row1">
+		<td class="color_header spacing_header" colspan=2>{$planet['Name']}: {$planet['Coord']}</td>
+		<td class="{$water_style}" {$bulle}>{$planet['percent_water']}</td>
+		<td colspan=9>&nbsp;</td>
 	</tr>
 ph;
         $this->PushOutput($out);
@@ -135,24 +137,24 @@ ph;
 //                .'<br/>Dans le bunker: '.$tb.'<br/>En sécurité: '.$tp.'%'
 //                .'<br/>Utilisation bunker: '.$tf.'%');
 
-        $this->Add_RessRow($row, $this->lng['row_stocks'], '', 'imperium_row0', $bulles);
+        $this->Add_RessRow($row, $this->lng['row_stocks'], '', 'row0', $bulles);
     }
 
-    public function Add_RessRow($data, $title, $key='',$style='imperium_header',$bulles=array()) {
+    public function Add_RessRow($data, $title, $key='',$style='header',$bulles=array()) {
         $out=<<<h
-	<TR id="{$style}">
-		<td id="{$style}">{$title}</td>
+	<TR class="color_{$style}">
+		<td class="color_header spacing_header">{$title}</td>
 h;
 
         foreach($this->keys as $vals) {
             $number = DataEngine::format_number($data["{$key}{$vals}"]);
             if ($bulles[$vals]) {
                 $out.=<<<r1
-		<td {$bulles["{$vals}"]} id="{$style}">{$number}</td>
+		<td class="spacing_{$style}" {$bulles["{$vals}"]}>{$number}</td>
 r1;
             } else {
                 $out.=<<<r2
-		<td id="{$style}">{$number}</td>
+		<td class="spacing_{$style}" >{$number}</td>
 r2;
             }
         }
@@ -161,31 +163,31 @@ r2;
 
         if ($bulles['total']) {
             $out.=<<<f1
-		<td {$bulles['total']} id="{$style}">{$number}</td>
+		<td class="spacing_row" {$bulles['total']}>{$number}</td>
 f1;
         } else {
             $out.=<<<f2
-		<td id="{$style}">{$number}</td>
+		<td class="spacing_row">{$number}</td>
 f2;
         }
-        $this->PushOutput($out."	</tr>");
+        $this->PushOutput($out."</tr>");
     }
 
-    public function Add_PercentRow($data, $title, $key='',$style='imperium_header',$bulles=array()) {
+    public function Add_PercentRow($data, $title, $key='',$style='header',$bulles=array()) {
         $out=<<<h
-	<TR id="{$style}">
-		<td id="{$style}">{$title}</td>
+	<TR class="color_{$style}">
+		<td class="color_header spacing_header">{$title}</td>
 h;
 
         foreach($this->keys as $vals) {
             $number = $data["{$key}{$vals}"].'%';
             if ($bulles[$vals]) {
                 $out.=<<<r1
-		<td {$bulles["{$vals}"]} id="{$style}">{$number}</td>
+		<td class="spacing_{$style}" {$bulles["{$vals}"]}>{$number}</td>
 r1;
             } else {
                 $out.=<<<r2
-		<td id="{$style}">{$number}</td>
+		<td class="spacing_{$style}">{$number}</td>
 r2;
             }
         }
@@ -194,11 +196,11 @@ r2;
 
         if ($bulles['total']) {
             $out.=<<<f1
-		<td {$bulles['total']} id="{$style}">{$number}</td>
+		<td class="spacing_row" {$bulles['total']}>{$number}</td>
 f1;
         } else {
             $out.=<<<f2
-		<td id="{$style}">{$number}</td>
+		<td class="spacing_row">{$number}</td>
 f2;
         }
         $this->PushOutput($out.'	</tr>');
@@ -207,19 +209,19 @@ f2;
 
     private function SetRowtplBatiments() {
         $this->currow = <<<ROW
-    <tr>
-        <td id="imperium_row%%class%%">%%Name%%</td>
-        <td id="imperium_row%%class%%">%%control%%</td>
-        <td id="imperium_row%%class%%">%%communication%%</td>
-        <td id="imperium_row%%class%%">%%university%%</td>
-        <td id="imperium_row%%class%%">%%technology%%</td>
-        <td id="imperium_row%%class%%">%%gouv%%</td>
-        <td id="imperium_row%%class%%">%%defense%%</td>
-        <td id="imperium_row%%class%%">%%shipyard%%</td>
-        <td id="imperium_row%%class%%">%%spacedock%%</td>
-        <td id="imperium_row%%class%%">%%bunker%%</td>
-        <td id="imperium_row%%class%%">%%tradepost%%</td>
-        <td id="imperium_row%%class%%">%%ressource%%</td>
+    <tr class="color_row%%class%%">
+        <td class="color_header spacing_row">%%Name%%</td>
+        <td class="spacing_row%%class%%">%%control%%</td>
+        <td class="spacing_row%%class%%">%%communication%%</td>
+        <td class="spacing_row%%class%%">%%university%%</td>
+        <td class="spacing_row%%class%%">%%technology%%</td>
+        <td class="spacing_row%%class%%">%%gouv%%</td>
+        <td class="spacing_row%%class%%">%%defense%%</td>
+        <td class="spacing_row%%class%%">%%shipyard%%</td>
+        <td class="spacing_row%%class%%">%%spacedock%%</td>
+        <td class="spacing_row%%class%%">%%bunker%%</td>
+        <td class="spacing_row%%class%%">%%tradepost%%</td>
+        <td class="spacing_row">%%ressource%%</td>
     </tr>
 ROW;
 
@@ -228,22 +230,22 @@ ROW;
         $this->curtpl = 'SetRowtplBatiments';        
         
         $this->currow = <<<ROW
-    <tr>
-        <td id="TDtableau" colspan=12>&nbsp;</td>
+    <tr class="color_bg">
+        <td colspan=12>&nbsp;</td>
     </tr>
-    <tr id="imperium_header">
-        <td id="TDtableau">{$this->lng['cols_planets']}</td>
-        <td id="TDtableau"><img src="%IMAGES_URL%control.gif" title="%%control%%"/></td>
-        <td id="TDtableau"><img src="%IMAGES_URL%communication.gif" title="%%communication%%"/></td>
-        <td id="TDtableau"><img src="%IMAGES_URL%university.gif" title="%%university%%"/></td>
-        <td id="TDtableau"><img src="%IMAGES_URL%technology.gif" title="%%technology%%"/></td>
-        <td id="TDtableau"><img src="%IMAGES_URL%gouv.gif" title="%%gouv%%"/></td>
-        <td id="TDtableau"><img src="%IMAGES_URL%defense.gif" title="%%defense%%"/></td>
-        <td id="TDtableau"><img src="%IMAGES_URL%shipyard.gif" title="%%shipyard%%"/></td>
-        <td id="TDtableau"><img src="%IMAGES_URL%spacedock.gif" title="%%spacedock%%"/></td>
-        <td id="TDtableau"><img src="%IMAGES_URL%bunker.gif" title="%%bunker%%"/></td>
-        <td id="TDtableau"><img src="%IMAGES_URL%tradepost.gif" title="%%tradepost%%"/></td>
-        <td id="TDtableau"><img src="%IMAGES_URL%ressource.gif" title="%%ressource%%"/></td>
+    <tr class="color_header spacing_header">
+        <td>{$this->lng['cols_planets']}</td>
+        <td><img src="%IMAGES_URL%control.gif" title="%%control%%"/></td>
+        <td><img src="%IMAGES_URL%communication.gif" title="%%communication%%"/></td>
+        <td><img src="%IMAGES_URL%university.gif" title="%%university%%"/></td>
+        <td><img src="%IMAGES_URL%technology.gif" title="%%technology%%"/></td>
+        <td><img src="%IMAGES_URL%gouv.gif" title="%%gouv%%"/></td>
+        <td><img src="%IMAGES_URL%defense.gif" title="%%defense%%"/></td>
+        <td><img src="%IMAGES_URL%shipyard.gif" title="%%shipyard%%"/></td>
+        <td><img src="%IMAGES_URL%spacedock.gif" title="%%spacedock%%"/></td>
+        <td><img src="%IMAGES_URL%bunker.gif" title="%%bunker%%"/></td>
+        <td><img src="%IMAGES_URL%tradepost.gif" title="%%tradepost%%"/></td>
+        <td cass="spacing_row"><img src="%IMAGES_URL%ressource.gif" title="%%ressource%%"/></td>
     </tr>
 ROW;
 
