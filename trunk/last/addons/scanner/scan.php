ac99412eb6d385479ddf0e71c74d0ea08eda57b3
Alex10336 <Alex10336@7e1166ec-126b-11df-b7f4-157485fe8b2f>
2010-05-05 16:41:59 +0000

    update issue 102 Une version du scanner qui marche vraiment ;)

--- trunk/last/addons/scanner/scan.php
+++ trunk/last/addons/scanner/scan.php
@@ -7,7 +7,6 @@
  **/
 
 require_once('../../init.php');
-//if (!defined('START')) define('START', microtime(true));
 require_once(INCLUDE_PATH.'Script.php');
 require_once(CLASS_PATH.'map.class.php'); // requis par ownuniverse.class
 require_once(CLASS_PATH.'ownuniverse.class.php'); // pour avoir les coords des planètes
@@ -21,7 +20,6 @@ if (!addons::getinstance()->Is_installed('scanner')) DataEngine::NoPermsAndDie()
 header('Content-Type: text/html;charset=utf-8');
 
 error_reporting(E_ALL);
-//@set_time_limit(0);
 FB::setEnabled(false);
 ob_end_flush();
 
@@ -30,10 +28,12 @@ if (!isset ($_GET['id']) || !isset ($_GET['step']) ||
     $_SESSION['scanner_email'] == '' || $_SESSION['scanner_session'] == '')
     DataEngine::ErrorAndDie('Paramètre(s) manquant(s)');
 
-// initialisation...
-$host = addons::getinstance()->ScanServer();
+/**
+ * @var scanner_addons 
+ */
+$scanner = addons::getinstance()->Get_Addons('scanner');
+$host = $scanner->ScanServer();
 
-//$host = 'test.zone';
 $header = 'Cookie: login_email='.str_replace('@', '%40', $_SESSION['scanner_email']).'; testcookie=1; PHPSESSID='.$_SESSION['scanner_session']."\r\n";
 $nbWormHole=0;
 $skipnb = intval($_GET['step']);
@@ -42,13 +42,12 @@ $skipnext = $skipnb+1;
 $carto = cartographie::getinstance();
 $coord = ownuniverse::getinstance()->get_comlevel();
 $coord = $coord[intval($_GET['id'])]['ss'];
-$coords = map::getinstance()->Parcours()->GetListeCoorByRay($coord, 5);
+$coords = map::getinstance()->Parcours()->GetListeCoorByRay($coord, $scanner->ScanRay('vortex'));
 $maxpage = count($coords)-1;
 $curcoord = $coords[$skipnb];
 
 echo '<html><head><title>Scan N°'.$skipnb.'/'.$maxpage.' (base:'.$coord.')</title></head><body>';
 echo 'Système '.$curcoord.':</br>';
-
 
 if ( ($page=GetUrl($host, '/galaxy/galaxy_overview.php?area=galaxy&starsystem_id='.$curcoord.'&fleet_id=&from=',$header)) ===false) die('error2, reload window game ?');
 
@@ -62,21 +61,13 @@ for ($i=0,$max=count($wormhole[1]);$i<$max;$i++) {
     else
         sleep(rand(5, 8));
     $nbWormHole++;
-    // $wormhole[1][$i] = 753
-    // $wormhole[1][$i] = 750eff6ccab9d274c32a4d0a7afdccc4
+    
     if ( ($page=GetUrl($host,'/wormhole/wormhole_info.php?side=end&fly_fleet_id=0&coordinate_select=undefined&wormhole_id='.$wormhole[1][$i].'&hash='.$wormhole[2][$i], $header)) === false) die('3');
-    //        $page = file_get_contents('data/galaxy_2.txt');
 
     preg_match_all("#ID Système stellaire.*\n.*\n.*>(\d+)</td>#",$page,$tmpId);
     preg_match_all("#Coordonnées.*\n.*\n.*>(\d+:\d+:\d+)</td>#",$page,$tmpCoord);
     if (isset($tmpId[1]) and isset($tmpCoord[1])) {
-        $tmp = $carto->add_vortex($tmpId[1][0].':'.$tmpCoord[1][0], $tmpId[1][1].':'.$tmpCoord[1][1], '', '', 'Automatique');
-//        if ($carto->Messages()>0 && $carto->Infos() == '')
-//            echo '<font color="red">'.strip_tags($carto->Erreurs(),$carto->Warns()).'</font></br>';
-//        else
-//            echo 'New vortex: '.$tmpId[1][0].':'.$tmpCoord[1][0].' => '.$tmpId[1][1].':'.$tmpCoord[1][1].'</br>';
-//
-//        $carto->reset();
+        $tmp = $carto->add_vortex($tmpId[1][0].':'.$tmpCoord[1][0], $tmpId[1][1].':'.$tmpCoord[1][1]);
     } else die('apologized');
 }
 $tmp = array();
@@ -85,9 +76,9 @@ $tmp[1] = intval($_GET['nbwormhole'])+$nbWormHole;
 $autoboink = 'false';
 
 if ($skipnext > $maxpage)
-    $base_url = ADDONS_URL.'scanner/index.php';
+    $base_url = './index.php';
 else {
-    $base_url = ADDONS_URL.'scanner/scan.php?id='.intval($_GET['id']).
+    $base_url = './scan.php?id='.intval($_GET['id']).
     '&step='.$skipnext.'&nbwormhole='.$tmp[1];
     $autoboink = 'true';
 }
