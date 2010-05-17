7347e386011c771355a3d14243da961dca03cf90
Alex10336 <Alex10336@7e1166ec-126b-11df-b7f4-157485fe8b2f>
2010-05-17 17:49:47 +0000

    ...

--- trunk/last/UPGRADE.sql
+++ trunk/last/UPGRADE.sql
@@ -2,4 +2,7 @@
 -- Fichier de MAJ SQL
 -- depuis 1.4.2
 
-ALTER TABLE `COA_Coordonnee` ADD `batiments` VARCHAR( 3 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL AFTER `water` 
+--
+-- NE PAS MODIFIER SUR LE SVN !
+-- http://code.google.com/p/eude/issues/detail?id=134
+-- 
