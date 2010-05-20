fe117fd193c0e93134a6034cdae5a2dbd81b0b82
Alex10336 <Alex10336@7e1166ec-126b-11df-b7f4-157485fe8b2f>
2010-05-20 23:30:01 +0000

    .

---
+++ addons/scanner/index.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * @Author: Alex10336
+ * Dernière modification: $Id$
+ * @license GNU Public License 3.0 ( http://www.gnu.org/licenses/gpl-3.0.txt )
+ * @license Creative Commons 3.0 BY-SA ( http://creativecommons.org/licenses/by-sa/3.0/deed.fr )
+ **/
+
+// Constantes par défaut:
+// define('IS_IMG',false);		// mode image
+// define('DEBUG_IMG',false);	// demande au navigateur de ne pas afficher comme une image
+// define('DEBUG_PLAIN',false);	// traitement comme étant du texte par le navigateur
+// define('USE_AJAX',false);	// mode xml
+// define('CHECK_LOGIN',true);	// désactive le besoin d'avoir un compte pour la page
+
+require_once('../../init.php');
+require_once(INCLUDE_PATH.'Script.php');
+require_once(CLASS_PATH.'map.class.php'); // requis par ownuniverse
+require_once(CLASS_PATH.'parser.class.php'); // requis par ownuniverse
+require_once(CLASS_PATH.'ownuniverse.class.php');
+
+// Check si activé
+if (!addons::getinstance()->Get_Addons('scanner')->CheckPerms()) DataEngine::NoPermsAndDie();
+
+//if (!isset ($_SESSION['scanner_email']) || $_SESSION['scanner_email'] == '')
+//    $_SESSION['scanner_email'] = 'votre@email.fr';
+
+if (isset($_POST['email']) && isset ($_POST['session'])) {
+    $_SESSION['scanner_email']   = gpc_esc($_POST['email']);
+    $_SESSION['scanner_session'] = gpc_esc($_POST['session']);
+}
+
+$ScannerEnabled = isset($_SESSION['scanner_email']) && isset ($_SESSION['scanner_session']) &&
+                    $_SESSION['scanner_email'] != '' && $_SESSION['scanner_session'] != '';
+
+require_once(TEMPLATE_PATH.'sample.tpl.php');
+$tpl = tpl_sample::getinstance();
+$tpl->css_file = false;
+$tpl->page_title = 'EU2: Addons scanner';
+
+$out = <<<form
+<form name="settings" action="index.php" method="POST">
+    Votre email: <input type="text" name="email" value="{$_SESSION['scanner_email']}" size="36" /><br/>
+    Votre id session: <input type="text" name="session" value="{$_SESSION['scanner_session']}" size="32" /><br/>
+    <br/>
+    L'id de session ce trouve dans le panneau des préférences: <br/>
+    - "<b>Vie Privée</b>"<br/>
+    - Règles de conservation: "<b>utiliser les paramètres personnalisés pour l'historique</b>"<br/>
+    - Le bouton "<b>Afficher les cookies...</b>" apparait.<br/>
+    - Dans le champ de recherche mettez "<b>australis</b>" ou "<b>borealis</b>".<br/>
+    - Sélectionner celui qui porte le nom de "<b>PHPSESSID</b>"<br/>
+    - Et copier/coller la partie "<b>Contenu</b>".<br/>
+    - la valeur ressemble a ça: <b>694000333b545d5ffda746955eb6067a</b><br/>
+    <input type="submit" value="Enregistrer" /><br/>
+</form>
+form;
+$tpl->PushOutput($out); // ajoute le texte précédant à la sortie qui sera affiché.
+
+$planets = ownuniverse::getinstance()->get_comlevelwithname();
+
+if ($ScannerEnabled && is_array($planets)) {
+    $tpl->PushOutput('<address>l\'exactitude des champ ci-dessus ne sont pas vérifié, et considéré systématiquement comme bon.</address><br/>');
+    foreach ($planets as $id => $coord)
+        $tpl->PushOutput('<a href="./scan.php?id='.$id.'&step=0">Vortex autour de la planète '.$coord['ss'].': '.$coord['name'].'</a><br/>');
+    foreach ($planets as $id => $coord)
+        $tpl->PushOutput('<a href="./planets.php?id='.$id.'&step=0">Planètes autour de la planète '.$coord['ss'].': '.$coord['name'].'</a><br/>');
+    
+} else {
+    $tpl->PushOutput('Pour la suite veuillez remplir les champs ci-dessus, ainsi que votre page perso "Production"');
+}
+// Un petit menu perso pour l'addons
+$menu = array(
+    'carte' => array('%ROOT_URL%Carte.php','%IMAGES_URL%Btn-Carte.png',125,'DataEngine::CheckPerms(AXX_MEMBER)', array()),
+    'prod' => array('%ROOT_URL%ownuniverse.php','%IMAGES_URL%Btn-Production.png',125,'DataEngine::CheckPerms(AXX_MEMBER)', array()),
+    'scanner' => array('%ADDONS_URL%scanner/index.php','%IMAGES_URL%test.png',125,'DataEngine::CheckPerms(AXX_MEMBER)', array()),
+);
+
+$tpl->DoOutput($menu,true); // stoppe toute execution du script et transmet les sorties html/xml/...
+// les deux 'true' étant
+// 1- Inclusion du menu (html, sans effet sur xml/img)
+// 2- Inclusion de l'entete de base (html, sans effet sur xml/img)
+
+
---
+++ addons/triangulation/index.php
@@ -0,0 +1,221 @@
+<?php
+/**
+ * @Author: Wilfried.Winner
+ * $Revision: Triangulation v1.3
+ * @license GNU Public License 3.0 ( http://www.gnu.org/licenses/gpl-3.0.txt )
+ * @license Creative Commons 3.0 BY-SA ( http://creativecommons.org/licenses/by-sa/3.0/deed.fr )
+ **/
+
+require_once('../../init.php');
+require_once(INCLUDE_PATH.'Script.php');
+require_once(CLASS_PATH.'map.class.php'); // requis par ownuniverse
+
+// Check si activé
+if (!addons::getinstance()->Get_Addons('triangulation')->CheckPerms()) DataEngine::NoPermsAndDie();
+if (isset($_POST['sys1']) && isset ($_POST['dist1'])&& isset($_POST['sys2']) && isset ($_POST['dist2']) && isset($_POST['sys3']) && isset ($_POST['dist3'])) {
+    $_SESSION['coord_syst1'] = gpc_esc($_POST['sys1']);
+    $_SESSION['ccdistance1'] = gpc_esc($_POST['dist1']);
+    $_SESSION['coord_syst2'] = gpc_esc($_POST['sys2']);
+    $_SESSION['ccdistance2'] = gpc_esc($_POST['dist2']);
+    $_SESSION['coord_syst3'] = gpc_esc($_POST['sys3']);
+    $_SESSION['ccdistance3'] = gpc_esc($_POST['dist3']);
+}
+$coordvalid = 1;
+ 
+$syst1= $_SESSION['coord_syst1']; 
+$dist1= $_SESSION['ccdistance1']; 
+$syst2= $_SESSION['coord_syst2'];
+$dist2= $_SESSION['ccdistance2'];
+$syst3= $_SESSION['coord_syst3'];
+$dist3= $_SESSION['ccdistance3'];
+
+require_once(TEMPLATE_PATH.'sample.tpl.php');
+$tpl = tpl_sample::getinstance();
+$tpl->page_title = 'EU2: Addons triangulation';
+
+
+$out = <<<form
+
+
+<font color='red', size='3',>
+<b>Attention!!</b> <br />Plus les distances en <i>[Pc]</i> séparant les centres de communications sont grandes plus la triangulation sera bonne <i>(min 5[Pc])</i></font>
+<br />
+<br />
+
+<font color=white>
+
+
+<form name="settings" action="index.php" method="POST">
+Pour remplir les cases ci-dessous, allez dans le jeu et dans le batiment centre de communication,<br />
+Puis faites des recherches avec le nom ou la planète du joueur (dans le jeu dans deux ou trois systèmes différents). <br />
+Remplissez ensuite le champs reservés ci-dessous avec le coodonnée du système dans lequel se trouve le centre de communication.<br /> 
+Remplissez aussi le champs reservés ci-dessous à la distance séparant le centre de communication à la planète sans son unité <i>[Pc]<i />.
+<br />
+<br />
+<i> Coordonnées du système du centre --------------------------- Distance en [Pc] séparant le centre de <br />
+de communications ----------------------------------------------- communication de la planète à trianguler</i><br /><br />
+    Coord. du syst. du centre de com. 1 : <input type="text" name="sys1" value="{$_SESSION['coord_syst1']}" size="16" />
+    distance sans unité entre CC1 et la planète : <input type="text" name="dist1" value="{$_SESSION['ccdistance1']}" size="16" /><br />
+    Coord. du syst. du centre de com. 2 : <input type="text" name='sys2' value="{$_SESSION['coord_syst2']}" size="16" />
+    distance sans unité entre CC2 et la planète : <input type="text" name="dist2" value="{$_SESSION['ccdistance2']}" size="16" /><br />
+    Coord. du syst. du centre de com. 3 : <input type="text" name="sys3" value="{$_SESSION['coord_syst3']}" size="16" />
+    distance sans unité entre CC3 et la planète : <input type="text" name="dist3" value="{$_SESSION['ccdistance3']}" size="16" /><br /> 
+ 
+    <br /> <font>L'exactitude des champ ci-dessus ne sont pas vérifiés, et considérés systématiquement comme bon.</font'><br />
+
+    <br /><font color=green size='4'><b>Triangulateur version beta </b> </font><br /><br />
+
+    Déterminer les coordonnées de la planète du joueur en cliquant sur le bouton "<b>Trianguler</b>". <br /><br />
+    <input type="submit" value="Trianguler" /><br /> <br /> 
+    
+</form>
+form;
+$tpl->PushOutput($out); // ajoute le texte précédant à la sortie qui sera affiché.
+
+if ($coordvalid) {
+//Debut de la triangulation
+/* 
+Dans le centre de communication la distance en Pc séparant deux système se calcul en faisant en utilisant la formule mathématique de la distance.
+Ainsi soit un système A(ax,ay) et système B(bx,by) la distance séparant des système est D = Arrondie{[(ax-bx)^2+(ay-by)^2]^(1/2),0} en posant 
+une condition sur les coordonnées 00 = 100; Prenons pour exemple le système A = 2456 ==> (ax=24, ay=56) et B = 3400 ==> (bx=34, by=00) 
+avec la condition sur les coordonnées 00 égale à 100, B = 3400 ==> (bx=34, by=100). La distance séparant les deux système vaut 
+D = Arrondie{[(24-34)^2+(56-100)^2]^(1/2),0} = 45[pc].
+A parte de cette équation et la mesure des distances obtenues avec au moins 2 au plus 3 centres de communitation du jeu EU2 se trouvant dans des 
+système différents nous pouvons retrouver les coordonnées de la planète dont on a déterminer la distance à l'aide des centres de communications.
+*/
+$systeme = array();
+$flag = 3;
+$Rep = 0;
+
+//L'une des coodonnees ou distance invalide. Triangulation faite en se basant sur quatre données
+if (($syst1 >= 1) && ($syst1 <= 10000) && ($dist1 >= 1) && ($dist1 <= 140) && 
+						($syst1 >= 1) && ($syst1 <= 10000) && ($dist1 >= 1) && ($dist1 <= 140) && 
+						($syst3 >= 1) && ($syst3 <= 10000) && ($dist3 >= 1) && ($dist3 <= 140) )
+	{
+		$cmpt = 1;//Pour compter le nombre de système probable
+		$flag = 2;
+		for ($i = 1; $i <= 10000; $i++) 
+		{
+			if ((calcdist($i,$syst1) == $dist1) && (calcdist($i,$syst2) == $dist2)&& (calcdist($i,$syst3) == $dist3)) 
+			{
+				$systeme[$cmpt++] = $i;
+				$flag = 0;
+			}
+		}$Rep = 0; 
+	}
+	else if(($syst1 <= 0) && ($syst1 >= 10000) || ($dist1 < 1) && ($dist1 > 140) && 
+					($syst2 >= 1) && ($syst2 <= 10000) && ($dist2 >= 1) && ($dist2 <= 140) && 
+					($syst3 >= 1) && ($syst3 <= 10000) && ($dist3 >= 1) && ($dist3 <= 140))
+	{	
+		$cmpt = 1;
+		$flag = 2; 
+		for ($i = 1; $i <= 10000; $i++) 
+		{
+			if ((calcdist($i,$syst2) == $dist2) && (calcdist($i,$syst3) == $dist3)) 
+			{
+				$systeme[$cmpt++] = $i;
+				$flag = 1;
+			}
+		}$Rep = 0;
+		
+	}else if (($syst2 <= 0) && ($syst2 >= 10000) || ($dist2 < 1) && ($dist2 > 140) && 
+						($syst1 >= 1) && ($syst1 <= 10000) && ($dist1 >= 1) && ($dist1 <= 140) && 
+						($syst3 >= 1) && ($syst3 <= 10000) && ($dist3 >= 1) && ($dist3 <= 140) )
+	{	
+		$cmpt = 1;
+		$flag = 2;
+		for ($i = 1; $i <= 10000; $i++) 
+		{
+			if ((calcdist($i,$syst1) == $dist1) && (calcdist($i,$syst3) == $dist3)) 
+			{
+				$systeme[$cmpt++] = $i;
+				$flag = 1;
+			}
+		}$Rep = 0;
+	
+	}else if (($syst3 <= 0) && ($syst3 >= 10000) || ($dist3 < 1) && ($dist3 > 140) && 
+						($syst1 >= 1) && ($syst1 <= 10000) && ($dist1 >= 1) && ($dist1 <= 140) && 
+						($syst3 >= 1) && ($syst3 <= 10000) && ($dist3 >= 1) && ($dist3 <= 140) )
+	{
+		$cmpt = 1;
+		$flag = 2;
+		for ($i = 1; $i <= 10000; $i++) 
+		{
+			if ((calcdist($i,$syst1) == $dist1) && (calcdist($i,$syst2) == $dist2)) 
+			{
+				$systeme[$cmpt++] = $i;
+				$flag = 1;
+			}
+		}$Rep = 0;
+	}	
+	 else
+	{
+		$tpl->PushOutput('Les données rentrées ne sont pas suffisant pour faire la triangulation.');
+		$tpl->PushOutput('<br />');
+		$Rep =1;
+	}
+	
+if ($flag == 0 && $Rep != 1)
+{
+		$tpl->PushOutput('La planète est dans le sytème : ');
+		for($i=1; $i < $cmpt; $i++)
+		{
+			$tpl->PushOutput($systeme[$i]);$tpl->PushOutput('<br />');
+			if ($i+1 < $cmpt)
+			$tpl->PushOutput(' ou <br />');
+	  }$Rep = 1;
+}
+else if ($flag == 1 && $Rep != 1)
+{
+		$tpl->PushOutput('La planète peut être dans le sytème : ');
+		for($i=1; $i < $cmpt; $i++)
+		{
+			$tpl->PushOutput($systeme[$i]);$tpl->PushOutput('<br />');
+			if ($i < $cmpt)
+			$tpl->PushOutput(' ou <br />');
+		}
+		$Rep = 1;
+}
+else if ($flag == 2 && $Rep != 1){
+		$tpl->PushOutput('Pas de solution, données incorrects.');
+		$tpl->PushOutput('<br />');
+		$Rep = 1;
+}
+else if ($Rep != 1){
+		$tpl->PushOutput('Les données rentrées ne sont invalides.');
+		$tpl->PushOutput('<br />');
+		$Rep = 1;
+	}
+	
+$out;
+} 
+else 
+$tpl->PushOutput('Pour la suite veuillez remplir les champs ci-dessus, ainsi que votre fiche "Production"');
+$tpl->PushOutput('<br />');$tpl->PushOutput('<br />');
+
+//------------------------------------------------------------------------------
+function carre($nbre) {
+return $nbre*$nbre;
+}
+
+function calcdist($sys1,$sys2) {
+//Pour avoir l'abscisse des coordonnées du système 1 [syst1 = 2456 ax = 2456%100]
+//Pour avoir l'ordonée des coordonnées du système 1 [syst1 = 2456 ay = (2456-2456%100)/100]
+	list($syst1y, $syst1x)=map::ss2xy($sys1);
+	list($syst2y, $syst2x)=map::ss2xy($sys2);
+	return round(round(round(sqrt(carre($syst1x-$syst2x)+carre($syst1y-$syst2y)),2),1));
+} 
+
+//------------------------------------------------
+// Un petit menu perso pour l'addons
+$menu = array(
+    'carte' => array('%ROOT_URL%Carte.php','%IMAGES_URL%Btn-Carte.png',125,'DataEngine::CheckPerms(AXX_MEMBER)', array()),
+    'prod' => array('%ROOT_URL%ownuniverse.php','%IMAGES_URL%Btn-Production.png',125,'DataEngine::CheckPerms(AXX_MEMBER)', array()),
+    'triangulation' => array('%ADDONS_URL%triangulation/index.php','%IMAGES_URL%Btn-triangulation.png',125,'DataEngine::CheckPerms(AXX_MEMBER)', array()));
+
+$tpl->DoOutput($menu,true); // stoppe toute execution du script et transmet les sorties html/xml/...
+// les deux 'true' étant
+// 1- Inclusion du menu (html, sans effet sur xml/img)
+// 2- Inclusion de l'entete de base (html, sans effet sur xml/img)
+
+
