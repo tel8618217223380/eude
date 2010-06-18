4c4a98a6c46f9afe186b997fa699ebbb7b564439
Alex10336 <Alex10336@7e1166ec-126b-11df-b7f4-157485fe8b2f>
2010-06-18 18:38:47 +0000

    nombre de troupes sur planète X a partir d'une flotte (1/2)

--- trunk/last/addons/grease/spooler.js
+++ trunk/last/addons/grease/spooler.js
@@ -2,6 +2,8 @@
 /// Dispacheur
 if (debug) AddToMotd('Page: '+c_page);
 
+if (c_page.indexOf('user/settings_overview.php?area=options')>0)      Options();
+
 if (GM_getValue(c_prefix+'actived','0')!='0') {
     if (c_page.indexOf('index.php')>0)                                  Index();
     if (c_page.indexOf('galaxy/galaxy_overview.php')>0)                Galaxy();
@@ -19,13 +21,16 @@ if (GM_getValue(c_prefix+'actived','0')!='0') {
 
     if (c_page.indexOf('fleet/fleet_info.php?')>0)                      Fleet();
     if (c_page.indexOf('fleet/commander_info.php?commander_id=')>0)   MaFiche();
-
+    if (c_page.indexOf('fleet/fleet_edit.php')>0)                   FleetEdit();
+    if (c_page.indexOf('fleet/fleet_troop.php')>0)                 FleetTroop();
+    
     if (c_page.indexOf('building/control/control_overview.php?area=planet')>0)
         ownuniverse();
     if (c_page.indexOf('battle/battle_ground_report_info.php?area=ground_battle')>0)
         troop_battle();
     if (c_page.indexOf('gamelog/gamelog_view.php?gamelog_id')>0)
         gamelog_spooler();
+
+    GM_setValue(c_prefix+'lastpage', c_page);
 }
 
-if (c_page.indexOf('user/settings_overview.php?area=options')>0)      Options();
