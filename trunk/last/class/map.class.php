63f5fb407c927839624f288441d770b31f02b29a
XCharismatic <XCharismatic@7e1166ec-126b-11df-b7f4-157485fe8b2f>
2010-07-07 10:30:15 +0000

    Fix & update issue 233

--- trunk/last/class/map.class.php
+++ trunk/last/class/map.class.php
@@ -392,7 +392,7 @@ class map /*extends parcours*/ {
         $where = 'WHERE '.$custom. ( ($this->inactif) ? '1=1 ':'`INACTIF`=0 ' ) . $where;
 
         $where = $where.' ORDER BY `POSIN` ASC';
-        $sql='SELECT a.`ID`, `TYPE`, `POSIN`, `POSOUT`, `USER`, `EMPIRE`,'.$if.' IFNULL(b.`Joueur`,"") as Joueur,IFNULL(c.`Grade`,"") as Grade FROM `SQL_PREFIX_Coordonnee` a left outer join `SQL_PREFIX_Membres` b on (a.`USER`=b.`Joueur`) left outer join `SQL_PREFIX_Grade` c on (b.`Grade`=c.`GradeId`) '.$where;
+        $sql='SELECT a.`ID`, a.`TYPE`, a.`POSIN`, a.`POSOUT`, a.`USER`, a.`EMPIRE`, '.$if.' IFNULL(b.`Joueur`,"") as Joueur,IFNULL(c.`Grade`,"") as Grade FROM `SQL_PREFIX_Coordonnee` a left outer join `SQL_PREFIX_Membres` b on (a.`USER`=b.`Joueur`) left outer join `SQL_PREFIX_Grade` c on (b.`Grade`=c.`GradeId`) '.$where;
         $mysql_result = DataEngine::sql($sql);
 
         return $mysql_result;
