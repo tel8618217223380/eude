2dc0066e704d1b3e2ccde83c68ca6ffc43f94c88
XCharismatic <XCharismatic@7e1166ec-126b-11df-b7f4-157485fe8b2f>
2010-07-24 10:18:29 +0000

    Oups

--- upgrade/upgrade142.sql
+++ upgrade/upgrade142.sql
@@ -9,55 +9,55 @@
 -- depuis 1.4.2
 --
 
-DROP TABLE `TEST_Membres_log`;
-ALTER TABLE `TEST_Coordonnee` CHANGE `water` `water` TIMESTAMP NULL DEFAULT NULL;
-ALTER TABLE `TEST_Coordonnee` CHANGE `water` `water` TINYINT( 3 ) UNSIGNED NULL DEFAULT NULL;
-ALTER TABLE `TEST_Coordonnee` ADD `batiments` TINYINT( 3 ) UNSIGNED NULL DEFAULT NULL AFTER `water`;
-ALTER TABLE `TEST_Coordonnee` CHANGE `troop` `troop` INT( 10 ) NULL DEFAULT '-1';
-UPDATE `TEST_Coordonnee` SET `troop` = NULL WHERE `troop` =  -1;
-ALTER TABLE `TEST_Coordonnee` CHANGE `troop` `troop` INT( 10 )  UNSIGNED NULL DEFAULT NULL;
-ALTER TABLE `TEST_Coordonnee` ADD  `troop_date` INT( 10 ) NOT NULL AFTER  `troop`;
+DROP TABLE `SQL_PREFIX_Membres_log`;
+ALTER TABLE `SQL_PREFIX_Coordonnee` CHANGE `water` `water` TIMESTAMP NULL DEFAULT NULL;
+ALTER TABLE `SQL_PREFIX_Coordonnee` CHANGE `water` `water` TINYINT( 3 ) UNSIGNED NULL DEFAULT NULL;
+ALTER TABLE `SQL_PREFIX_Coordonnee` ADD `batiments` TINYINT( 3 ) UNSIGNED NULL DEFAULT NULL AFTER `water`;
+ALTER TABLE `SQL_PREFIX_Coordonnee` CHANGE `troop` `troop` INT( 10 ) NULL DEFAULT '-1';
+UPDATE `SQL_PREFIX_Coordonnee` SET `troop` = NULL WHERE `troop` =  -1;
+ALTER TABLE `SQL_PREFIX_Coordonnee` CHANGE `troop` `troop` INT( 10 )  UNSIGNED NULL DEFAULT NULL;
+ALTER TABLE `SQL_PREFIX_Coordonnee` ADD  `troop_date` INT( 10 ) NOT NULL AFTER  `troop`;
 
  
-ALTER TABLE `TEST_Grade` CHANGE `Niveau` `Niveau` SMALLINT( 5 ) UNSIGNED NOT NULL;
-ALTER TABLE `TEST_Grade` CHANGE `Rattachement` `Rattachement` SMALLINT( 5 ) UNSIGNED NOT NULL;
-ALTER TABLE `TEST_itineraire` CHANGE `Start` `Start` SMALLINT( 5 ) UNSIGNED NOT NULL;
-ALTER TABLE `TEST_itineraire` CHANGE `End` `End` SMALLINT( 5 ) UNSIGNED NOT NULL;
-ALTER TABLE `TEST_Log` CHANGE `DATE` `DATE` TIMESTAMP NOT NULL;
-ALTER TABLE `TEST_Log` CHANGE `LOGIN` `LOGIN` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
-ALTER TABLE `TEST_Membres` CHANGE `Points` `Points` INT( 10 ) UNSIGNED NOT NULL;
-ALTER TABLE `TEST_Membres` CHANGE `Date` `Date` TIMESTAMP NOT NULL;
-ALTER TABLE `TEST_Membres` CHANGE `Economie` `Economie` SMALLINT( 3 ) UNSIGNED NOT NULL;
-ALTER TABLE `TEST_Membres` CHANGE `Commerce` `Commerce` SMALLINT( 3 ) UNSIGNED NOT NULL;
-ALTER TABLE `TEST_Membres` CHANGE `Recherche` `Recherche` SMALLINT( 3 ) UNSIGNED NOT NULL;
-ALTER TABLE `TEST_Membres` CHANGE `Combat` `Combat` SMALLINT( 3 ) UNSIGNED NOT NULL;
-ALTER TABLE `TEST_Membres` CHANGE `Construction` `Construction` SMALLINT( 3 ) UNSIGNED NOT NULL;
-ALTER TABLE `TEST_Membres` CHANGE `Navigation` `Navigation` SMALLINT( 3 ) UNSIGNED NOT NULL;
-ALTER TABLE `TEST_Membres` CHANGE `Grade` `Grade` SMALLINT( 5 ) UNSIGNED NOT NULL;
-ALTER TABLE `TEST_Membres` CHANGE `pts_architecte` `pts_architecte` INT( 10 ) UNSIGNED NOT NULL;
-ALTER TABLE `TEST_Membres` CHANGE `pts_mineur` `pts_mineur` INT( 10 ) UNSIGNED NOT NULL;
-ALTER TABLE `TEST_Membres` CHANGE `pts_science` `pts_science` INT( 10 ) UNSIGNED NOT NULL;
-ALTER TABLE `TEST_Membres` CHANGE `pts_commercant` `pts_commercant` INT( 10 ) UNSIGNED NOT NULL;
-ALTER TABLE `TEST_Membres` CHANGE `pts_amiral` `pts_amiral` INT( 10 ) UNSIGNED NOT NULL;
-ALTER TABLE `TEST_Membres` CHANGE `pts_guerrier` `pts_guerrier` INT( 10 ) UNSIGNED NOT NULL;
-ALTER TABLE `TEST_Users` CHANGE `Permission` `Permission` SMALLINT( 4 ) UNSIGNED NOT NULL DEFAULT '0';
+ALTER TABLE `SQL_PREFIX_Grade` CHANGE `Niveau` `Niveau` SMALLINT( 5 ) UNSIGNED NOT NULL;
+ALTER TABLE `SQL_PREFIX_Grade` CHANGE `Rattachement` `Rattachement` SMALLINT( 5 ) UNSIGNED NOT NULL;
+ALTER TABLE `SQL_PREFIX_itineraire` CHANGE `Start` `Start` SMALLINT( 5 ) UNSIGNED NOT NULL;
+ALTER TABLE `SQL_PREFIX_itineraire` CHANGE `End` `End` SMALLINT( 5 ) UNSIGNED NOT NULL;
+ALTER TABLE `SQL_PREFIX_Log` CHANGE `DATE` `DATE` TIMESTAMP NOT NULL;
+ALTER TABLE `SQL_PREFIX_Log` CHANGE `LOGIN` `LOGIN` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+ALTER TABLE `SQL_PREFIX_Membres` CHANGE `Points` `Points` INT( 10 ) UNSIGNED NOT NULL;
+ALTER TABLE `SQL_PREFIX_Membres` CHANGE `Date` `Date` TIMESTAMP NOT NULL;
+ALTER TABLE `SQL_PREFIX_Membres` CHANGE `Economie` `Economie` SMALLINT( 3 ) UNSIGNED NOT NULL;
+ALTER TABLE `SQL_PREFIX_Membres` CHANGE `Commerce` `Commerce` SMALLINT( 3 ) UNSIGNED NOT NULL;
+ALTER TABLE `SQL_PREFIX_Membres` CHANGE `Recherche` `Recherche` SMALLINT( 3 ) UNSIGNED NOT NULL;
+ALTER TABLE `SQL_PREFIX_Membres` CHANGE `Combat` `Combat` SMALLINT( 3 ) UNSIGNED NOT NULL;
+ALTER TABLE `SQL_PREFIX_Membres` CHANGE `Construction` `Construction` SMALLINT( 3 ) UNSIGNED NOT NULL;
+ALTER TABLE `SQL_PREFIX_Membres` CHANGE `Navigation` `Navigation` SMALLINT( 3 ) UNSIGNED NOT NULL;
+ALTER TABLE `SQL_PREFIX_Membres` CHANGE `Grade` `Grade` SMALLINT( 5 ) UNSIGNED NOT NULL;
+ALTER TABLE `SQL_PREFIX_Membres` CHANGE `pts_architecte` `pts_architecte` INT( 10 ) UNSIGNED NOT NULL;
+ALTER TABLE `SQL_PREFIX_Membres` CHANGE `pts_mineur` `pts_mineur` INT( 10 ) UNSIGNED NOT NULL;
+ALTER TABLE `SQL_PREFIX_Membres` CHANGE `pts_science` `pts_science` INT( 10 ) UNSIGNED NOT NULL;
+ALTER TABLE `SQL_PREFIX_Membres` CHANGE `pts_commercant` `pts_commercant` INT( 10 ) UNSIGNED NOT NULL;
+ALTER TABLE `SQL_PREFIX_Membres` CHANGE `pts_amiral` `pts_amiral` INT( 10 ) UNSIGNED NOT NULL;
+ALTER TABLE `SQL_PREFIX_Membres` CHANGE `pts_guerrier` `pts_guerrier` INT( 10 ) UNSIGNED NOT NULL;
+ALTER TABLE `SQL_PREFIX_Users` CHANGE `Permission` `Permission` SMALLINT( 4 ) UNSIGNED NOT NULL DEFAULT '0';
 -- 
-DELETE FROM `TEST_Coordonnee` WHERE `inactif`=1;
-ALTER TABLE `TEST_Coordonnee` DROP `INACTIF`;
+DELETE FROM `SQL_PREFIX_Coordonnee` WHERE `inactif`=1;
+ALTER TABLE `SQL_PREFIX_Coordonnee` DROP `INACTIF`;
 
 
-ALTER TABLE `TEST_Coordonnee` ADD `udate` INT( 10 ) UNSIGNED NOT NULL;
-UPDATE `TEST_Coordonnee` SET `udate`=UNIX_TIMESTAMP(`DATE`);
-ALTER TABLE  `TEST_Coordonnee` DROP `DATE`;
-ALTER TABLE `TEST_Coordonnee` ADD `troop_udate` INT( 10 ) UNSIGNED NULL;
-UPDATE `TEST_Coordonnee` SET `troop_udate`=UNIX_TIMESTAMP(`troop_date`);
-ALTER TABLE  `TEST_Coordonnee` DROP `troop_date`;
-ALTER TABLE  `TEST_Coordonnee` CHANGE `troop_udate` `troop_date` INT( 10 ) UNSIGNED NULL;
+ALTER TABLE `SQL_PREFIX_Coordonnee` ADD `udate` INT( 10 ) UNSIGNED NOT NULL;
+UPDATE `SQL_PREFIX_Coordonnee` SET `udate`=UNIX_TIMESTAMP(`DATE`);
+ALTER TABLE  `SQL_PREFIX_Coordonnee` DROP `DATE`;
+ALTER TABLE `SQL_PREFIX_Coordonnee` ADD `troop_udate` INT( 10 ) UNSIGNED NULL;
+UPDATE `SQL_PREFIX_Coordonnee` SET `troop_udate`=UNIX_TIMESTAMP(`troop_date`);
+ALTER TABLE  `SQL_PREFIX_Coordonnee` DROP `troop_date`;
+ALTER TABLE  `SQL_PREFIX_Coordonnee` CHANGE `troop_udate` `troop_date` INT( 10 ) UNSIGNED NULL;
 
 
-INSERT INTO `TEST_Config` (`key`, `value`) VALUES('config', 'a:9:{s:7:"version";s:5:"1.4.5";s:9:"ForumLink";s:31:"https://code.google.com/p/eude/";s:11:"CanRegister";s:1:"1";s:12:"DefaultGrade";s:1:"3";s:8:"MyEmpire";s:0:"";s:17:"Parcours_Max_Time";i:0;s:16:"Parcours_Nearest";i:5;s:8:"eude_srv";s:12:"australis.fr";s:6:"closed";s:1:"0";}');
+INSERT INTO `SQL_PREFIX_Config` (`key`, `value`) VALUES('config', 'a:9:{s:7:"version";s:5:"1.4.5";s:9:"ForumLink";s:31:"https://code.google.com/p/eude/";s:11:"CanRegister";s:1:"1";s:12:"DefaultGrade";s:1:"3";s:8:"MyEmpire";s:0:"";s:17:"Parcours_Max_Time";i:0;s:16:"Parcours_Nearest";i:5;s:8:"eude_srv";s:12:"australis.fr";s:6:"closed";s:1:"0";}');
 
-CREATE TABLE `TEST_troops_attack` (
+CREATE TABLE `SQL_PREFIX_troops_attack` (
   `ID` int(11) NOT NULL auto_increment,
   `type` enum('defender','attacker') NOT NULL,
   `nb_assault` int(11) NOT NULL,
@@ -75,7 +75,7 @@ CREATE TABLE `TEST_troops_attack` (
 
 
 
-CREATE TABLE `TEST_troops_pillage` (
+CREATE TABLE `SQL_PREFIX_troops_pillage` (
   `pid` int(11) NOT NULL auto_increment,
   `mid` int(11) NOT NULL,
   `Player` varchar(30) NOT NULL,
@@ -99,34 +99,34 @@ CREATE TABLE `TEST_troops_pillage` (
 -- -- -- -- -- -- -- --  Partie split =)  -- -- -- -- -- -- -- -- -- -- -- -- --
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
-CREATE TABLE `TEST_Coordonnee_Joueurs` as SELECT `id` `jID`,`USER`,`EMPIRE`,`INFOS`,`batiments`,`troop`,`troop_date` FROM `TEST_Coordonnee`;
-ALTER TABLE `TEST_Coordonnee_Joueurs` ADD PRIMARY KEY ( `jID` );
-ALTER TABLE `TEST_Coordonnee_Joueurs` ADD INDEX ( `batiments` );
-ALTER TABLE `TEST_Coordonnee_Joueurs` ADD INDEX ( `troop` );
+CREATE TABLE `SQL_PREFIX_Coordonnee_Joueurs` as SELECT `id` `jID`,`USER`,`EMPIRE`,`INFOS`,`batiments`,`troop`,`troop_date` FROM `SQL_PREFIX_Coordonnee`;
+ALTER TABLE `SQL_PREFIX_Coordonnee_Joueurs` ADD PRIMARY KEY ( `jID` );
+ALTER TABLE `SQL_PREFIX_Coordonnee_Joueurs` ADD INDEX ( `batiments` );
+ALTER TABLE `SQL_PREFIX_Coordonnee_Joueurs` ADD INDEX ( `troop` );
 
 
-CREATE TABLE `TEST_Coordonnee_Planetes2` as
-SELECT IFNULL(p.pID, c.ID) as pID, Titane,Cuivre,Fer,Aluminium,Mercure,Silicium,Uranium,Krypton,Azote,Hydrogene,c.`water` FROM `TEST_Coordonnee` c
- LEFT JOIN `TEST_Coordonnee_Planetes` p on (c.`ID`=p.`pID`);
-DROP TABLE `TEST_Coordonnee_Planetes`;
-RENAME TABLE `TEST_Coordonnee_Planetes2` TO `TEST_Coordonnee_Planetes`;
+CREATE TABLE `SQL_PREFIX_Coordonnee_Planetes2` as
+SELECT IFNULL(p.pID, c.ID) as pID, Titane,Cuivre,Fer,Aluminium,Mercure,Silicium,Uranium,Krypton,Azote,Hydrogene,c.`water` FROM `SQL_PREFIX_Coordonnee` c
+ LEFT JOIN `SQL_PREFIX_Coordonnee_Planetes` p on (c.`ID`=p.`pID`);
+DROP TABLE `SQL_PREFIX_Coordonnee_Planetes`;
+RENAME TABLE `SQL_PREFIX_Coordonnee_Planetes2` TO `SQL_PREFIX_Coordonnee_Planetes`;
 
 -- omfg
-ALTER TABLE `TEST_Coordonnee_Planetes` CHANGE `pID` `pID` INT( 10 ) UNSIGNED NOT NULL;
-ALTER TABLE `TEST_Coordonnee_Planetes` ADD PRIMARY KEY ( `pID` );
+ALTER TABLE `SQL_PREFIX_Coordonnee_Planetes` CHANGE `pID` `pID` INT( 10 ) UNSIGNED NOT NULL;
+ALTER TABLE `SQL_PREFIX_Coordonnee_Planetes` ADD PRIMARY KEY ( `pID` );
 --
 
-ALTER TABLE `TEST_Coordonnee` DROP `USER`,DROP `EMPIRE`,DROP `INFOS`,DROP `water`,DROP `batiments`,DROP `troop`,DROP `troop_date`;
+ALTER TABLE `SQL_PREFIX_Coordonnee` DROP `USER`,DROP `EMPIRE`,DROP `INFOS`,DROP `water`,DROP `batiments`,DROP `troop`,DROP `troop_date`;
 
 -- Version 1.4.5.1
-ALTER TABLE `TEST_Coordonnee` ADD INDEX ( `udate` );
-ALTER TABLE `TEST_Coordonnee` CHANGE `TYPE` `TYPE` TINYINT( 2 ) NOT NULL DEFAULT '3';
-ALTER TABLE `TEST_Coordonnee` ADD INDEX ( `TYPE` );
+ALTER TABLE `SQL_PREFIX_Coordonnee` ADD INDEX ( `udate` );
+ALTER TABLE `SQL_PREFIX_Coordonnee` CHANGE `TYPE` `TYPE` TINYINT( 2 ) NOT NULL DEFAULT '3';
+ALTER TABLE `SQL_PREFIX_Coordonnee` ADD INDEX ( `TYPE` );
 
-ALTER TABLE `TEST_Coordonnee_Joueurs` ADD INDEX ( `USER` );
-ALTER TABLE `TEST_Coordonnee_Joueurs` ADD INDEX ( `EMPIRE` );
-ALTER TABLE `TEST_Coordonnee_Joueurs` ADD INDEX ( `INFOS` );
-ALTER TABLE `TEST_Coordonnee_Joueurs` ADD INDEX ( `batiments` ) ;
-ALTER TABLE `TEST_Coordonnee_Joueurs` ADD INDEX ( `troop` ) ;
+ALTER TABLE `SQL_PREFIX_Coordonnee_Joueurs` ADD INDEX ( `USER` );
+ALTER TABLE `SQL_PREFIX_Coordonnee_Joueurs` ADD INDEX ( `EMPIRE` );
+ALTER TABLE `SQL_PREFIX_Coordonnee_Joueurs` ADD INDEX ( `INFOS` );
+ALTER TABLE `SQL_PREFIX_Coordonnee_Joueurs` ADD INDEX ( `batiments` ) ;
+ALTER TABLE `SQL_PREFIX_Coordonnee_Joueurs` ADD INDEX ( `troop` ) ;
 
-ALTER TABLE `TEST_Coordonnee_Planetes` ADD INDEX ( `water` ) ;
+ALTER TABLE `SQL_PREFIX_Coordonnee_Planetes` ADD INDEX ( `water` ) ;
