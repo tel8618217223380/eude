fff31a5c4fc4c04c82561da2d469c73af9962111
Alex10336 <Alex10336@7e1166ec-126b-11df-b7f4-157485fe8b2f>
2010-07-26 20:36:01 +0000

    MAJ i18n-en

--- tpl/lng/en/ownuniverse.lng.php
+++ tpl/lng/en/ownuniverse.lng.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @author Alex10336
- * @translator
+ * @translator Curtis
  * Dernière modification: $Id$
  * @license GNU Public License 3.0 ( http://www.gnu.org/licenses/gpl-3.0.txt )
  * @license Creative Commons 3.0 BY-SA ( http://creativecommons.org/licenses/by-sa/3.0/deed.fr )
@@ -12,74 +12,74 @@
 // -- ownuniverse.php ----------------------------------------------------------
 
 $lng = array();
-$lng['page_title']     = 'EU2: Son univers';
-$lng['data_empty']     = 'Information non remplie par le joueur !';
-$lng['start_info']     = 'Lisez les infos sur le <b>i</b> pour commencer';
-$lng['data_error']     = 'Information collé non reconnue';
-$lng['control_center_error'] = 'Le centre de contrôle en premier !';
-$lng['universe_added'] = 'Informations de votre univers ajouté.';
-$lng['universe_updated'] = 'Informations de votre univers mise a jour.';
-$lng['universe_nochange'] = 'Aucune différence, donnée inchangée.';
+$lng['page_title']     = 'EU2: Production';
+$lng['data_empty']     = 'Information Not Entered !';
+$lng['start_info']     = 'Read info <b>i</b> to start';
+$lng['data_error']     = 'Information not recognized';
+$lng['control_center_error'] = 'Access Your Control Center For Information !';
+$lng['universe_added'] = 'Your Control Center Information has been added';
+$lng['universe_updated'] = 'Your Control Center Information has been updated';
+$lng['universe_nochange'] = 'No change';
 // partie détection, par block,
-$lng['control_center_ident'] = 'population supply per day';
+$lng['control_center_ident'] = 'Population supply each day';
 $lng['block_planet_0'] = 'Description';
-$lng['block_planet_1'] = 'Overall';
+$lng['block_planet_1'] = 'Total';
 $lng['block_coords_0'] = 'Coordinates';
-$lng['block_coords_1'] = 'Distance to';
-$lng['block_batiments_0'] = 'Building (extension levels)';
+$lng['block_coords_1'] = 'Distance';
+$lng['block_batiments_0'] = 'Building (Level Of Development)';
 $lng['block_batiments_1'] = 'Resources';
 $lng['block_ress_0'] = $lng['block_batiments_1'];
 $lng['block_ress_1'] = 'Production per hour';
 $lng['block_prod_0'] = $lng['block_ress_1'];
-$lng['block_prod_1'] = 'Resources in bunker';
+$lng['block_prod_1'] = 'Resources in the bunker';
 $lng['block_bunker_0'] = $lng['block_prod_1'];
-$lng['block_bunker_1'] = 'population supply per day';
+$lng['block_bunker_1'] = 'Population supply each day';
 $lng['block_sell_0'] = $lng['block_bunker_1'];
-$lng['block_sell_1'] = 'Estimated next supply';
+$lng['block_sell_1'] = 'Next supply';
 
-$lng['planet_error'] = 'Cette planète ne fait pas partie de votre univers (voir centre de controle)';
-$lng['planet_added']    = 'Information de la planète %s ajouté.';
-$lng['planet_nochange'] = 'Aucune différence, donnée inchangée.';
+$lng['planet_error']    = 'This planet is not yours (See Control Center)';
+$lng['planet_added']    = 'Information of %s Added';
+$lng['planet_nochange'] = 'No change';
 // partie détection,
-$lng['planet_ident'] = 'Resource distribution';
-$lng['planet_key_0'] = 'Resource distribution';
-$lng['planet_key_1'] = 'Planet information';
-$lng['planet_key_2'] = 'Resource distribution';
+$lng['planet_ident'] = 'Resource Details';
+$lng['planet_key_0'] = 'Resource Details';
+$lng['planet_key_1'] = 'Planet Information';
+$lng['planet_key_2'] = 'Resource Details';
 $lng['planet_key_3'] = 'Coordinates';
-$lng['planet_key_4'] = 'Water surface';
+$lng['planet_key_4'] = 'Surface d\'Water';
 
 // Tableau...
 $lng['tips_header'] = <<<tips_header
-            Coller ici les détails du '<b>Centre de contrôle</b>' puis '<b>Planètes</b>'<br/>
-(Ctrl+A puis Ctrl+C après avoir ouvert la page)<br/>
+            Paste Details Here '<b>Control Center</b>' then '<b>Planets</b>'<br/>
+(Ctrl+A then Ctrl+C after you open the page)<br/>
 <br/>
-Suivit après par planète un copier/coller de la page 'Aperçu' pour les informations complémentaire
+ copy/paste for more information see preview
 tips_header;
 
-$lng['header']            = 'Ajout de l\'info... (centre de contrôle niveau 2 mini)';
-$lng['btn_submit']        = 'Interpréter';
+$lng['header']            = 'Add info... (Control Center Level 2 minimum)';
+$lng['btn_submit']        = 'Add';
 $lng['btn_reset']         = 'Reset';
 
-$lng['row_concentration'] = 'Concentration';
+$lng['row_concentration'] = 'Percentage';
 $lng['row_prod/h']        = 'Prod/h';
 $lng['row_stocks']        = 'Stocks';
 $lng['row_Total']         = 'Total';
-$lng['row_race_needed']   = 'Consommation';
-$lng['cols_planets']      = 'Planète(s)';
+$lng['row_race_needed']   = 'Consumption';
+$lng['cols_planets']      = 'Planet(s)';
 $lng['cols_Total']        = 'Total';
 
 // bulles
 $lng['current_ress_row_1']=<<<crr1
 %s<br/>
-Sur planète: %s<br/>
-Dans le bunker: %s
+On Planet: %s<br/>
+In Bunker: %s
 crr1;
 
 $lng['current_ress_row_2']=<<<crr2
-Sur planète: %s
-<br/>Dans le bunker: %s
-<br/>En sécurité: %s%%
-<br/>Utilisation bunker: %s%%
+On Planet: %s
+<br/>In Bunker: %s
+<br/>Safe: %s%%
+<br/>Bunker Useage: %s%%
 crr2;
 
 
