8301c257aecfae5e4d44aa3e7cc31bc5210da93d
Alex10336 <Alex10336@7e1166ec-126b-11df-b7f4-157485fe8b2f>
2010-08-30 01:02:22 +0000

    le dossier cache qui va avec....

---
+++ last/cron.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @author Alex10336
+ * Dernière modification: $Id$
+ * @license GNU Public License 3.0 ( http://www.gnu.org/licenses/gpl-3.0.txt )
+ * @license Creative Commons 3.0 BY-SA ( http://creativecommons.org/licenses/by-sa/3.0/deed.fr )
+ */
+define('IS_IMG', true);
+define('CHECK_LOGIN', false);
+include('init.php');
+require_once(INCLUDE_PATH . 'Script.php');
+require_once(CLASS_PATH . 'img.class.php');
+
+include_once(INCLUDE_PATH . 'cron_jobs.php');
+
+if (IS_IMG)
+    img::Create(1, 1)->SetColorHexa('000000')->Fill()->Render();
